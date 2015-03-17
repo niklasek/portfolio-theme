db35fd844397c5b236a0012768627539b727ca2d
Niklas Ekman <niklasek@users.noreply.github.com>
2015-03-17 13:45:15 +0200

    Fixade registreringen av menyerna

--- functions.php
+++ functions.php
@@ -1,8 +1,6 @@
 <?php
-$themename= 'portfolio-theme';
-// This theme uses wp_nav_menu() in two location.  
 register_nav_menus( array(  
-  'primary' => __( 'Main Menu', $themename )
+  'main_menu' => 'Site navigation'
 ) );
 
 add_theme_support( 'post-thumbnails' );
